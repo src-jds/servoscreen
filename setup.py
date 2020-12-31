c67766b103bf3db7cf256b5a5d777f27b3368919
GIT-JoshDS <13048116+GIT-JoshDS@users.noreply.github.com>
2020-12-31 11:28:53 +1100

    Updated setup.py and requirements to reflect current status.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='servoscreen',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='GNU General Public License v3.0',
+    author='Joshua Dunkley-Smith',
+    author_email='',
+    description='Servo-i (HBO) CIE-based remote display program'
+)
